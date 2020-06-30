7c2ccc9bf59636c198af708bd1aeb73ef80958ce
João Tribuzy <jvtribuzy@gmail.com>
2020-06-30 17:21:07 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by João Tribuzy on 30/06/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

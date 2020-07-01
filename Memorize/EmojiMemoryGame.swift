f903cc802d94566ec939cfc5998e654241fe6f1b
João Tribuzy <jvtribuzy@gmail.com>
2020-07-01 19:59:48 -0400

    Loading emojis on correct size on cards

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -10,13 +10,8 @@ import SwiftUI
 class EmojiMemoryGame: ObservableObject {
     @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
-    static var emojis: Array<String> {
-        var emojiList: Array<String> = []
-        for i in 0x1F601...0x1F64F { emojiList.append(String(UnicodeScalar(i) ?? "-")) }
-        return emojiList
-    }
-    
     static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🕷", "🎃", "🕸"]
         return MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
             return emojis[pairIndex]
         }

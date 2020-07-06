b77a2ba0f7ab3fb81a5e7d8fa9689b85b2287402
João Tribuzy <jvtribuzy@gmail.com>
2020-07-06 12:55:49 -0400

    Loading pairs of emojis in a Grid

--- Memorize/ViewModel/EmojiMemoryGame.swift
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -12,7 +12,7 @@ class EmojiMemoryGame: ObservableObject {
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String> = ["👻", "🕷", "🎃", "🕸"]
-        return MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
+        return MemoryGame<String>(numberOfPairsOfCards: 3) { pairIndex in
             return emojis[pairIndex]
         }
     }
